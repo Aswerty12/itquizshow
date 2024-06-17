24171177ec3454fe68adc59ea8292b29eb8a7733
Jean Luc <biboy.janda@gmail.com>
2024-06-17 14:27:45 +0800

    Routing and 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,26 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PlayerComponent } from './player/player.component';
+import { HostComponent } from './host/host.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {   path: '',
+        component: HomeComponent,
+        title: 'Home page'},
+    {   path: 'host',
+        component: HostComponent,
+        title : 'Hosting'
+    },
+    {
+        path: 'play',
+        component: PlayerComponent,
+        title : 'Playing'
+    },
+    {
+        path: '**',
+        component: PagenotfoundComponent,
+        title : "404 Error"
+    }
+];
