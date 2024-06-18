3040495ff65ccdb34354c36da9a40f87b4ab73fb
Jean Luc <biboy.janda@gmail.com>
2024-06-18 14:29:15 +0800

    Added about, improved css

--- src/app/pagenotfound/pagenotfound.component.ts
+++ src/app/pagenotfound/pagenotfound.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { HomeComponent } from '../home/home.component';
-import { AboutComponent } from '../about/about.component';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pagenotfound',
   standalone: true,
-  imports: [HomeComponent,AboutComponent,RouterLink],
+  imports: [RouterLink],
   templateUrl: './pagenotfound.component.html',
   styleUrl: './pagenotfound.component.scss'
 })
